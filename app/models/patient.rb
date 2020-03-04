aec5696e825009293237d950940d254ab1147326
mlisareyes <mlisareyes@gmail.com>
2020-03-04 13:18:02 -0500

    clean up

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,6 @@
 class Patient < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments
-  # has_many :notes
-  # has_many :users_notes, through: :notes, source: :user
   has_many :notes
 
   validates :name, presence: true
